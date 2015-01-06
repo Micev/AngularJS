d7831b0f40a21e75b17d146486eb0e54a8bb8b98
micev <micevgeorgi@gmail.com>
2015-01-06 23:36:50 +0200

    add login and register routing

--- AngularAdsProject/js/app.js
+++ AngularAdsProject/js/app.js
@@ -1,9 +1,19 @@
-var app = angular.module('adsApp', ['ngRoute']);
+var app = angular.module('adsApp', ['ngRoute', 'ngResource']);
+
+app.constant('baseServiceUrl', 'http://softuni-ads.azurewebsites.net/api/');
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/',{
         templateUrl: 'template/home.html',
         controller: 'HomeController'
+    });
+    $routeProvider.when('/login', {
+        templateUrl: 'template/login.html',
+        controller: 'LoginController'
+    });
+    $routeProvider.when('/register', {
+        templateUrl: 'template/register.html',
+        controller: 'RegisterController'
     });
     $routeProvider.otherwise({
         redirectTo: '/'
