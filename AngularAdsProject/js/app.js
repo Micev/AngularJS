e9eea98c2f8004bfe29cac6e372b4e4ebce4d50d
micev <micevgeorgi@gmail.com>
2015-01-10 02:14:08 +0200

    add notificationProvider

--- AngularAdsProject/js/app.js
+++ AngularAdsProject/js/app.js
@@ -1,8 +1,8 @@
-var app = angular.module('adsApp', ['ngRoute', 'ngResource', 'LocalStorageModule']);
+var app = angular.module('adsApp', ['ngRoute', 'ngResource', 'LocalStorageModule', 'angular-extended-notifications']);
 
 app.constant('baseServiceUrl',  'http://softuni-ads.azurewebsites.net/api/');
 
-app.config(['$routeProvider','localStorageServiceProvider', function($routeProvider, localStorageServiceProvider){
+app.config(['$routeProvider','localStorageServiceProvider', 'notificationsProvider', function($routeProvider, localStorageServiceProvider, notificationsProvider){
     $routeProvider.when('/',{
         templateUrl: 'template/home.html',
         controller: 'HomeController'
@@ -15,9 +15,21 @@ app.config(['$routeProvider','localStorageServiceProvider', function($routeProvi
         templateUrl: 'template/register.html',
         controller: 'RegisterController'
     });
+    $routeProvider.when('/logout',{
+
+    });
     $routeProvider.otherwise({
         redirectTo: '/'
     });
 
     localStorageServiceProvider.setStorageType('localStorage');
+
+    notificationsProvider.setDefaults({
+        // set the directory containing the html templates
+        templatesDir: 'node_modules/angular-extended-notifications/templates/',
+        // use font-awesome icons
+        faIcons: true,
+        // close notifications on route change
+        closeOnRouteChange: 'route' // or 'state' using uiRouter…
+    });
 }]);
